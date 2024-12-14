a1a531e70512bf4fea4a75b15b88cf1ae48def91
Smigola <qoqoshvili.bacho@gmail.com>
2024-12-14 22:54:33 +0400

    test

---
+++ test.py
@@ -0,0 +1,50 @@
+import random
+
+
+def user_input(): #bachana
+    pass
+    #davanamatot card change funqcionali
+
+
+class Player: #bachana
+    def __init__(self,name):
+        self.name = name
+
+    def remove_player():
+        pass 
+
+
+class Card:
+    #card_list_value = [
+     #   1,2,3,{"J": 11},{"D":12} #all
+    #]
+
+    #card_list_colour = []
+
+    def __init__(self):
+        pass
+
+    def grade_calculation(): #daviti
+        pass
+        #if tie:
+        #extra_comparison()
+
+    def extra_comparison(): #daviti
+        pass
+
+    def card_change(): #mariami
+        pass
+    
+    def show_cards(): #mariami
+        pass
+
+
+
+
+def main():
+    while True: 
+        pass
+
+
+if __name__ == "__main__":
+    main()
